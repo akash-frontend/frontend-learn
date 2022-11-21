a740f648ffef9eaee59093b61dd94f6f33ee0d65
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2022-11-21 16:01:20 +0530

    Update main.jsx

--- Day26/src/main.jsx
+++ Day26/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App3";
+import App from "./App";
 import ContextEx from "./ContextEx";
 import "./index.css";
 import TodoApp from "./TodoApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <TodoApp />
+     <App /> 
+     {/* <TodoApp />*/}
     {/* <ContextEx /> */}
   </React.StrictMode>
 );
